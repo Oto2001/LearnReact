4824ac8065e0f81ae661360d39a6e10af3e949aa
otomamatsashvili <otar.mamatsashvili@gmail.com>
2021-12-11 19:35:19 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Setup variables
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const booksArray = [
+	{
+		id: 1,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/51j8IiHZUtL._SX329_BO1,204,203,200_.jpg',
+		title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones Hardcover',
+		author: 'James Clear',
+	},
+	{
+		id: 2,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/51FBhyphRKL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+		title: 'Call Us What We Carry: Poems',
+		author: 'Amanda Gorman',
+	},
+	{
+		id: 3,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/51csugYPrJL._SX331_BO1,204,203,200_.jpg',
+		title: 'Interesting Stories For Curious People: A Collection of Fascinating Stories About History, Science, Pop Culture and Just About Anything Else You Can Think of',
+		author: "Bill O'Neill",
+	},
+];
+
+function BookList(book) {
+	return (
+		<section className="booklist">
+			{booksArray.map((book) => {
+				// const { img, title, author } = booksArray;
+				return <Book key={book.id} {...book}></Book>;
+			})}
+		</section>
+	);
+}
+
+const Book = ({ id, img, title, author }) => {
+	return (
+		<article className="book">
+			<img src={img} alt="alt" />
+			<h1>{title}</h1>
+			<h4>{author}</h4>
+		</article>
+	);
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));
